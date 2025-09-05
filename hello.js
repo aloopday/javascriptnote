90c95466a5ecd576e54ff0771b2a30a39bd601be
aloopday <aloopday@gmail.com>
2025-09-05 11:53:27 +0800

    add some js

---
+++ hello.js
@@ -0,0 +1,2 @@
+console.log("Hello,World!");
+console.log("Hello, Mr Charles, you will get more profits from the Hong Kong . Trust your self.")
---
+++ myscript.js
@@ -0,0 +1,4 @@
+
+function myFunction(){
+    document.getElementById("demo").innerHTML="Best service in Hong Kong";
+}
